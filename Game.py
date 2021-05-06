dbd3de8e4026623a57a5a5b25224b544c8bdc50e
malloryeastburn <mallorykaitlin@gmail.com>
2021-05-06 08:57:24 -0500

    Added dealer hand

--- Game.py
+++ Game.py
@@ -13,16 +13,18 @@ def acesCheck(total, aCount):
         total -= 10
         aCount -= 1
     return total
-def points():
+
+def points(who):
     total = 0
-    for card in playerHand:
+    for card in who:
         if card == 'A':
             total += 11
         elif card == "J" or card == "Q" or card == "K":
             total += 10
         else:
             total += int(card)
-    return acesCheck(total,playerHand.count("A"))
+    return acesCheck(total,who.count("A"))
+
 def reShuffle():
     cards.clear()
     for index in range(len(DECK)):
@@ -32,27 +34,52 @@ def reShuffle():
     dealerHand.clear()
     playerTotal = 0
     dealerTotal = 0
+    hitMe(playerHand)
+    hitMe(dealerHand)
+    hitMe(playerHand)
+    hitMe(dealerHand)
     
-def hitMe():
+def hitMe(who):
     card = random.randint(0,len(cards)-1)
-    print(card)
-    playerHand.append(str(cards[card]))
+    who.append(str(cards[card]))
     counts[card] -= 1
     if counts[card] < 0:
         cards.remove(cards[card])
         counts.remove(counts[card])
     return
-    
-hitMe()
-hitMe()
-playerTotal = points()
-print("You have a " + str(playerHand) + ".")
-print("You have " + str(playerTotal) + " points.")
-answer = ""
-while (playerTotal) < 21 and answer != "s":
-    answer = input("What would you like to do? (h, s): ")
-    if answer == "h":
-        hitMe()
-        playerTotal = points()
-        print("You have a " + str(playerHand) + ".")
-        print("You have " + str(playerTotal) + " points.")
+playAgain = True
+while playAgain:  
+    reShuffle()
+    playerTotal = points(playerHand)
+    dealerTotal = points(dealerHand)
+    print("You have a " + str(playerHand) + ".")
+    print("You have " + str(playerTotal) + " points.")
+    print("Dealer has a '" + str(dealerHand[0]) + "' showing.")
+    answer = ""
+    while (playerTotal) < 21 and answer != "s":
+        answer = input("What would you like to do? (h, s): ")
+        if answer == "h":
+            hitMe(playerHand)
+            playerTotal = points(playerHand)
+            print("You have a " + str(playerHand) + ".")
+            print("You have " + str(playerTotal) + " points.")
+            if playerTotal > 21:
+                print("BUST! Dealer wins!")
+        elif answer == "s":
+            print("Dealer has " + str(dealerHand) + ".")
+            if dealerTotal >= playerTotal:
+                print("Dealer wins!")
+            else:
+                while dealerTotal <= 17 or dealerTotal <= playerTotal:
+                    hitMe(dealerHand)
+                    dealerTotal = points(dealerHand)
+                    print("Dealer has " + str(dealerHand) + ".")
+                    print("Dealer has " + str(dealerTotal) + " points.")
+                    if dealerTotal > 21:
+                        print("Dealer BUSTS! You win!")
+                    elif dealerTotal <= 21 and dealerTotal > playerTotal:
+                        print("Dealer wins!")
+    answer = input("Would you like to play again? y/n: ")
+    if answer == "n":
+        playAgain = False
+        print("Thanks for playing!")
